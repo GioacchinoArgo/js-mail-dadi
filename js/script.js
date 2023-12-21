97faddfa76b01010f1abd84d3e3f0dd838b64d11
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2023-12-21 19:05:38 +0100

    completed first exercise

--- js/script.js
+++ js/script.js
@@ -1,1 +1,42 @@
 console.log('JS OK')
+
+// - 1
+const play = document.getElementById('play');
+const userVictory = document.getElementById('user-winner');
+const userLose = document.getElementById('user-loser');
+const userDraw = document.getElementById('user-draw');
+const userNumber = document.getElementById('user-number')
+const computerVictory = document.getElementById('computer-winner');
+const computerLose = document.getElementById('computer-loser');
+const computerDraw = document.getElementById('computer-draw');
+const computerNumber = document.getElementById('computer-number')
+console.log (play, userVictory, userLose, computerVictory, computerLose);
+
+play.addEventListener('click', function(){
+    
+    // - 3 
+    let resultUser = '';
+    let resultComputer = '';
+    console.log (resultUser, resultComputer, userNumber);
+
+    // - 4 
+    resultUser = Math.floor(Math.random() * 6) + 1;
+    console.log('Result User', resultUser);
+    userNumber.innerText = resultUser;
+    resultComputer = Math.floor(Math.random() * 6) + 1;
+    console.log('Result Computer', resultComputer);
+    computerNumber.innerText = resultComputer;
+
+    // - 5
+    if (resultUser > resultComputer) {
+        userVictory.classList.remove('d-none')
+        computerLose.classList.remove('d-none')
+    } else if (resultUser < resultComputer) {
+        userLose.classList.remove('d-none')
+        computerVictory.classList.remove('d-none')
+    } else if (resultUser === resultComputer) {
+        userDraw.classList.remove('d-none')
+        computerDraw.classList.remove('d-none')
+    }
+    return;
+})
